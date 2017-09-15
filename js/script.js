57ad6d96d6876776b36731c023b93b71a753b9d5
efriedla <efriedla20852@gmail.com>
2017-09-15 14:31:49 -0700

    please don't delete my work

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
 /*TO DO LIST
+1. better alter for game over
+2 add sharks going side to side
+3. 
 5. add animated bubbles to player one
 6. add sound
 */
@@ -10,22 +13,25 @@ var y = 650; //vertical
 var dx = 8; //volocity speed
 var dy = 8; 
 var w = 30;
-var h = 40;
+var h = 45;
 var cW;
 var cH;
 var player1;
 var scorebored= document.getElementById("scorebored");
 var score = 0;
 var r = 35;
+/*var waterSound = new Audio("auto/water.mp3");
+var splashSound = new Audio("auto/splash1.mp3");
+var cowboySound = new Audio("auto/cowboy.mp3");*/
 var fish = [
-       { "id":"little1", "x":100,"y":-20,"w":90,"h":90, "r": 70, "ox":100,"oy":-20,"ow":90,"oh":90, "or": 70},
-       { "id":"little2", "x":525,"y":-50,"w":20,"h":20, "r": 20, "ox":525,"oy":-50,"ow":20,"oh":20, "or": 20},
-       { "id":"little3", "x":350,"y":-160,"w":20,"h":20, "r": 20, "ox":350,"oy":-160,"ow":20,"oh":20, "or": 20},
-       { "id":"little4", "x":450,"y":-190,"w":30,"h":30, "r": 30, "ox":450,"oy":-190,"ow":30,"oh":30, "or": 30},
-       { "id":"little5", "x":150,"y":-230,"w":80,"h":80, "r": 60, "ox":150,"oy":-230,"ow":80,"oh":80, "or": 60},
-       { "id":"little6", "x":550,"y":-370,"w":35,"h":35, "r": 35, "ox":550,"oy":-300,"ow":35,"oh":35, "or": 35},
-       { "id":"little7", "x":350,"y":-420,"w":40,"h":40, "r": 50, "ox":350,"oy":-320,"ow":40,"oh":40, "or": 40},
-       { "id":"little8", "x":250,"y":-530,"w":70,"h":70, "r": 50, "ox":250,"oy":-390,"ow":70,"oh":70, "or": 50},
+       { "id":"little1", "x":100,"y":-40,"w":90,"h":95, "r": 80, "ox":100,"oy":-90,"ow":90,"oh":95, "or": 70},
+       { "id":"little2", "x":525,"y":-50,"w":20,"h":25, "r": 20, "ox":525,"oy":-100,"ow":20,"oh":25, "or": 20},
+       { "id":"little3", "x":350,"y":-160,"w":20,"h":25, "r": 20, "ox":350,"oy":-160,"ow":20,"oh":25, "or": 20},
+       { "id":"little4", "x":450,"y":-190,"w":20,"h":25, "r": 20, "ox":450,"oy":-190,"ow":20,"oh":25, "or": 20},
+       { "id":"little5", "x":150,"y":-230,"w":80,"h":90, "r": 60, "ox":150,"oy":-230,"ow":80,"oh":85, "or": 60},
+       { "id":"little6", "x":550,"y":-370,"w":15,"h":25, "r": 35, "ox":550,"oy":-300,"ow":15,"oh":20, "or": 15},
+       { "id":"little7", "x":350,"y":-420,"w":40,"h":50, "r": 40, "ox":350,"oy":-320,"ow":40,"oh":45, "or": 40},
+       { "id":"little8", "x":250,"y":-530,"w":70,"h":80, "r": 50, "ox":250,"oy":-390,"ow":70,"oh":75, "or": 50},
 
         ];
 window.onload =function() {
@@ -36,20 +42,16 @@ window.onload =function() {
     cH = ctx.canvas.height;
    /* return setInterval(draw, 20); *///calls draw function
 console.log("loaded");
+ //waterSound.play();
 }
-
-
 //fishes swim down screen
 function renderFish(){
     for(var i = 0; i < fish.length; i++){
         var enemy = document.getElementById("enemy");
         ctx.drawImage(enemy, fish[i].x , fish[i].y+=.8, fish[i].w, fish[i].h);
-      
-
    }
-    //console.log("enemy fish x=" + fish[0].x + ", y=" + fish[0].y);
 }
-
+//CREATE PLAYER ONE
 function renderPlayer1(){
   var fish2 = document.getElementById('fish2');
     ctx.drawImage(fish2, x, y, w, h);
@@ -71,6 +73,7 @@ function pressArrowKeys (e) {
             x += dx;
             break;
 
+
     }
 }
 
@@ -83,23 +86,26 @@ var checkForCollision = function(x1, y1, fish) {
   if (distance < fish.r) {
    // IF FISH EATEN IT GET BIGGER AND RESPOND RANDOMLY ON THE PAGE
     fish.x = Math.floor((Math.random() * 690) + 1);
-    fish.y = -45;
-     fish.w += 10;
-    fish.h += 10;
-    fish.r += 5;
-   
-
+    fish.y = Math.floor((Math.random() * -290) - 20);
+     fish.w += 15;
+    fish.h += 15;
+    fish.r += 8;
+    for(var i = 0; i < fish.length; i++){
+        console.log(fish[i]);
+    }
     CheckForSize(fish);
     //fish gets too far it will come back AND COME BAKC A LITLE BIGGER
-  } else if(distance > 1100){
-    fish.y = -45;
+  } else if(distance > 1000){
     fish.x = Math.floor((Math.random() * 690) + 1);
+    fish.y = Math.floor((Math.random() * -290) - 20);
      fish.w += 1;
     fish.h += 1;
     fish.r += 1;
+
   }
   // CheckForSize();
   return distance;
+
 };
 
 function endGame(){
@@ -107,8 +113,10 @@ function endGame(){
   x = 350;
   y = 650;
   w = 30;
-  h = 40;
+  h = 45;
   r = 45;
+ // cowboySound.play();
+
    scorebored.textContent = "Score: " + score;
    //ADD THE SIZE BACK TO ORIGINAL SIZE
    for(var i = 0; i < fish.length; i++){
@@ -122,9 +130,12 @@ function endGame(){
 
 //check size of fish
 var CheckForSize = function(fish){
-    if ( fish.w > w && fish.h > h){
+    if ( fish.w >= w && fish.h >= h){
       console.log ('gulp!');
-      alert("game over");
+      //alert("game over");
+      swal(  'Gulp...',
+  'BETTER LUCK NEXT TIME!',
+  'error');
       endGame();
       // newGame();
     }else /*(fish[i].w < w && fish[i].h < h) */{
@@ -133,7 +144,7 @@ var CheckForSize = function(fish){
       r += 3;
       score += 1;
      
-      console.log(fish);
+     // console.log(fish);
       //ADD SCORE
       scorebored.textContent = "Score: " + score;
       console.log("grew");
@@ -156,9 +167,8 @@ function draw() {
    checkForCollision(x, y, fish[7]);
    renderPlayer1();
    renderFish();
- 
+   
 
 }
 
  setInterval(draw, 30); 
-
